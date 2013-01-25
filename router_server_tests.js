a429aee00e5b9df3bc3d4f18793563553db19987
Tom Coleman <tom@thesnail.org>
2013-01-25 13:17:31 +1100

    Fixed up tests to deal with latest meteor

--- router_server_tests.js
+++ router_server_tests.js
@@ -2,63 +2,70 @@
 //
 // it seems that stream does in it's tests.
 Tinytest.add("Simple Router.serve", function(test) {
-  Meteor.Router.add('/foo', function() {
+  Meteor.Router.add('/server/foo', function() {
     return 'data';
   });
   
-  Meteor.Router.add(/page\/(\d+)/, function(number) {
+  Meteor.Router.add(/server\/page\/(\d+)/, function(number) {
     return [number, 'page'];
   });
   
-  var resp = Meteor.http.get('http://localhost:3000/foo')
+  var resp = Meteor.http.get('http://localhost:3000/server/foo')
   test.equal(resp.content, 'data');
   
-  var resp = Meteor.http.get('http://localhost:3000/page/7')
+  var resp = Meteor.http.get('http://localhost:3000/server/page/7')
   test.equal(resp.statusCode, 7);
   test.equal(resp.content, 'page');
 });
 
 Tinytest.add("Router.serve with params", function(test) {
-  Meteor.Router.add('/bar/:id.xml', function(id) {
+  Meteor.Router.add('/server/bar/:id.xml', function(id) {
     return id;
   })
   
-  var resp = Meteor.http.get('http://localhost:3000/bar/content.xml')
+  var resp = Meteor.http.get('http://localhost:3000/server/bar/content.xml')
   test.equal(resp.content, 'content');
 });
 
 
 Tinytest.add("Router.serve various response types", function(test) {
   Meteor.Router.add({
-    '/baz-1': [404, {'x-my-header': 'Baz'}, 'data'],
-    '/baz-2': [405, 'data'],
-    '/baz-3': ['data'],
-    '/baz-4': 406,
-    '/baz-5': 'data'
+    '/server/baz-1': [404, {'x-my-header': 'Baz'}, 'data'],
+    '/server/baz-2': [405, 'data'],
+    '/server/baz-3': ['data'],
+    '/server/baz-4': 406,
+    '/server/baz-5': 'data'
   });
   
-  var resp = Meteor.http.get('http://localhost:3000/baz-1')
+  var resp = Meteor.http.get('http://localhost:3000/server/baz-1')
   test.equal(resp.statusCode, 404);
   test.equal(resp.content, 'data');
   test.equal(resp.headers['x-my-header'], 'Baz');
   
-  var resp = Meteor.http.get('http://localhost:3000/baz-2')
+  // grab it again to make sure we aren't messing with it
+  var resp = Meteor.http.get('http://localhost:3000/server/baz-1')
+  test.equal(resp.statusCode, 404);
+  test.equal(resp.content, 'data');
+  test.equal(resp.headers['x-my-header'], 'Baz');
+  
+  var resp = Meteor.http.get('http://localhost:3000/server/baz-2')
   test.equal(resp.statusCode, 405);
   test.equal(resp.content, 'data');
   
-  var resp = Meteor.http.get('http://localhost:3000/baz-3')
+  var resp = Meteor.http.get('http://localhost:3000/server/baz-3')
   test.equal(resp.content, 'data');
   
-  var resp = Meteor.http.get('http://localhost:3000/baz-4')
+  var resp = Meteor.http.get('http://localhost:3000/server/baz-4')
   test.equal(resp.statusCode, 406);
   
-  var resp = Meteor.http.get('http://localhost:3000/baz-5')
+  var resp = Meteor.http.get('http://localhost:3000/server/baz-5')
   test.equal(resp.content, 'data');
 });
 
 
 Tinytest.add("Router.serve with futures", function(test) {
-  Meteor.Router.add('/delayed', function() {
+  Meteor.Router.add('/server/delayed', function() {
+    var Future = __meteor_bootstrap__.require('fibers/future');
     var fut = new Future();
     setTimeout(function() {
       fut.ret('foo-in-timeout');
@@ -67,6 +74,6 @@ Tinytest.add("Router.serve with futures", function(test) {
     return fut.wait();
   });
   
-  var resp = Meteor.http.get('http://localhost:3000/delayed')
+  var resp = Meteor.http.get('http://localhost:3000/server/delayed')
   test.equal(resp.content, 'foo-in-timeout');
 });
