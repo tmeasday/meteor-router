607345efd557427af1389fa3e846f3633c01a4ff
Tarang Patel <Tarangp@users.noreply.github.com>
2014-04-11 15:44:05 +0200

    Update for Blaze

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
 });
 
 Npm.depends({
-  'connect': '2.7.10'
+  'connect': '2.9.0'
 });
 
 Package.on_use(function (api, where) {
@@ -14,17 +14,17 @@ Package.on_use(function (api, where) {
   api.use('page-js-ie-support', 'client');
   api.use('underscore', ['client', 'server']);
   
+  // for backward compat before Meteor linker changes
+  if (typeof api.export !== 'undefined') {
+    api.use('webapp', 'server');
+    api.use('ui', 'client', {weak: true}); //For helpers
+    api.use('HTML5-History-API', 'client', {weak: true});
+  }
+  
   api.add_files('lib/router_client.js', 'client');
   api.add_files('lib/router_helpers.js', 'client');
   api.add_files('lib/router_server.js', 'server');
   api.add_files('lib/router_common.js', ['client', 'server']);
-  
-  // for backward compat before Meteor linker changes
-  if (typeof api.export !== 'undefined') {
-    api.use('webapp', 'server');
-    
-    api.use('HTML5-History-API', 'client', {weak: true});
-  }
 });
 
 
